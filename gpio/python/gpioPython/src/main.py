cf3ad3c975c660a1e3499e4b698333b8fd900ba4
Allan Henrique Kamimura <allan.kamimura@toradex.com>
2024-06-17 16:42:42 -0300

    gpio: Python: change gpioChip to match a generic /dev/gpiochip* number

--- gpio/python/gpioPython/src/main.py
+++ gpio/python/gpioPython/src/main.py
@@ -2,38 +2,39 @@
 
 import time
 import gpiod
+from glob import glob
 
 if __name__ == "__main__":
 
     gpiolineName = None # "SODIMM_55" - Example of pin A0 on Aster Carrier Board
-    gpioChip = "/dev/gpiochip0"
+    gpioChip = None # "/dev/gpiochip0"  - Example of pin A0 on Aster Carrier Board
 
-    if gpiolineName == None:
+    if (gpiolineName == None) or (gpioChip == None):
+        for gpioChip in glob("/dev/gpiochip*"):
+            chip = gpiod.Chip(gpioChip)
+            print(f"\n{chip.name()} - {chip.num_lines()} lines")
+            
+            for line_offset in range(chip.num_lines()):
+                line = chip.get_line(line_offset)
+                line_name = line.name()
+                if line_name == None:
+                    line_name = "unnamed  "
 
-        chip = gpiod.Chip(gpioChip)
-        print(f"{chip.name()} - {chip.num_lines()} lines")
-        
-        for line_offset in range(chip.num_lines()):
-            line = chip.get_line(line_offset)
-            line_name = line.name()
-            if line_name == None:
-                line_name = "unnamed  "
+                if line.is_used():
+                    line_consumer = line.consumer()
+                else: 
+                    line_consumer = "unused"
 
-            if line.is_used():
-                line_consumer = line.consumer()
-            else: 
-                line_consumer = "unused"
+                if line.direction() == "1":
+                    line_direction = "input"
+                else:  
+                    line_direction = "output"
+                print(f"Line {line_offset:<2}:  {line_name:<15}  {line_consumer:<20}  {line_direction:<10}")
 
-            if line.direction() == "1":
-                line_direction = "input"
-            else:  
-                line_direction = "output"
-            print(f"Line {line_offset}:  {line_name}  {line_consumer}  {line_direction}")
+                #line_is_open_source = line.is_open_source()
+                #line_is_open_drain = line.is_open_drain()
 
-            #line_is_open_source = line.is_open_source()
-            #line_is_open_drain = line.is_open_drain()
-
-            line.release()
+                line.release()
     else:
         gpioline = gpiod.find_line(gpiolineName)
 
